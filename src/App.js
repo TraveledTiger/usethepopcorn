d0fc2d20a574952e6db785e1a597ce8e918da008
Samuel <sambrown2002@outlook.com>
2026-04-07 20:26:49 -0400

    add: focus on searchbar on 'enter' and on startup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
+
 import StarRating from "./StarRating";
 
 const average = (arr) =>
@@ -163,6 +163,26 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery],
+  );
+
   return (
     <input
       className="search"
@@ -170,6 +190,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
